57092944d0b2d2b986c9f9917715e86fc7a4386c
wout <wout@impinc.co.uk>
2013-01-08 21:46:32 +0100

    Added pluing and readme

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-svg.easing.js
-=============
+# svg.easing.js
 
-Easing equations for the fx module in the svg.js library
+Additional easing equations for the fx module in the [svgjs.com](http://svgjs.com) library.
+
+Svg.easing.js is licensed under the terms of the MIT License.
+
+## Usage
+Include this plugin after including the svg.js library in your html document. To use the custom easing methods:
+
+```javascript
+var draw = svg('paper').size(400, 400);
+var rect = draw.rect(100, 100);
+
+rect.animate(500, SVG.easing.bounce).move(300, 300);
+```
+
+Available easing methods are:
+- `SVG.easing.bounce`
+- `SVG.easing.backIn`
+- `SVG.easing.backOut`
---
+++ svg.easing.js
@@ -0,0 +1,40 @@
+// svg.easing.js 0.1 - Copyright (c) 2013 Wout Fierens Licensed under the MIT license
+
+SVG.easing = {
+  
+  backIn: function (pos) {
+    var s = 1.70158;
+    return pos * pos * ((s + 1) * pos - s);
+  },
+  
+  backOut: function (pos) {
+    pos = pos - 1;
+    var s = 1.70158;
+    return pos * pos * ((s + 1) * pos + s) + 1;
+  },
+  
+  bounce: function(pos) {
+    var s = 7.5625,
+        p = 2.75,
+        l;
+        
+    if (pos < (1 / p)) {
+      l = s * pos * pos;
+    } else {
+      if (pos < (2 / p)) {
+        pos -= (1.5 / p);
+        l = s * pos * pos + .75;
+      } else {
+        if (pos < (2.5 / p)) {
+          pos -= (2.25 / p);
+          l = s * pos * pos + .9375;
+        } else {
+          pos -= (2.625 / p);
+          l = s * pos * pos + .984375;
+        }
+      }
+    }
+    return l;
+  }
+  
+};
